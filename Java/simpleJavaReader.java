1f3638a7c6b1fbb7c7f40c68a21db2ad4f30566e
nimeshkrishnani <nimesh.krishnani@iiitb.org>
2019-05-08 18:26:12 +0530

    java reader added

---
+++ Java/simpleJavaReader.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String temp = br1.readLine();
+		System.out.println(temp);
+	}
+}
